c6cc8f0b7cdfc8b96770683c6ce0ff60050a9906
Yokko405 <hiyume.2@gmail.com>
2024-03-28 19:28:32 +0900

    ホーム画面/レシピ詳細画面の作成、モデル作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,12 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  def new
+    @recipe = Recipe.new
+  end
+
 end
