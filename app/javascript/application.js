8ef8d769055bb1b1904a90970830f9e60f0552b9
Yokko405 <hiyume.2@gmail.com>
2024-04-02 12:30:39 +0900

    起動時とログアウト時のエラー解消

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as Rails from "@rails/ujs";
-
+// import * as Rails from "@rails/ujs";
+// Rails.start();
+import "recipes"
 
--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:migrate
-bundle exec rake db:drop db:create db:migrate RAILS_ENV=production
+bundle exec rake db:migrate
+# bundle exec rake db:drop db:create db:migrate RAILS_ENV=production
