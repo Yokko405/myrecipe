991c6512d6f9299caed41254a704f361aa67ec0d
Yokko405 <hiyume.2@gmail.com>
2024-04-03 21:09:55 +0900

    ヘッダー

--- app/javascript/recipes.js
+++ app/javascript/recipes.js
@@ -56,4 +56,23 @@ document.addEventListener('turbo:load', () => {
     });
   }
 
+  'レシピ画像のプレビュー'
+  const inputElement = document.getElementById('image-preview');
+  inputElement.addEventListener('change', function() {
+    const previewContainer = document.getElementById('preview-container');
+    const previewImage = document.getElementById('image-preview-tag');
+    const file = inputElement.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        previewImage.src = e.target.result;
+        previewImage.style.display = 'block';
+      }
+      reader.readAsDataURL(file);
+    } else {
+      previewImage.style.display = 'none';
+      previewImage.src = '';
+    }
+  });
 });
